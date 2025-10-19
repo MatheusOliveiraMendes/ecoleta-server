00a96c8e9e5ca1fc49853b8ca79b821ceae2d6f5
Matheus Mendes <matheus-need@hotmail.com>
2025-10-19 19:58:20 +0100

    change img back to front

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,15 +1,26 @@
 import { Request } from 'express';
 
+const stripTrailingSlash = (value: string): string => value.replace(/\/+$/, '');
+
 export function resolveBaseUrl(req: Request): string {
     const fromEnv = process.env.APP_URL || process.env.RENDER_EXTERNAL_URL;
     if (fromEnv) {
-        return fromEnv;
+        return stripTrailingSlash(fromEnv);
     }
 
     const host = req.get('host');
     if (host) {
-        return `${req.protocol}://${host}`;
+        return stripTrailingSlash(`${req.protocol}://${host}`);
     }
 
     return 'http://localhost:3333';
+}
+
+export function resolveAssetsBaseUrl(req: Request): string {
+    const fromEnv = process.env.ASSETS_BASE_URL;
+    if (fromEnv) {
+        return stripTrailingSlash(fromEnv);
+    }
+
+    return `${resolveBaseUrl(req)}/uploads`;
 }
